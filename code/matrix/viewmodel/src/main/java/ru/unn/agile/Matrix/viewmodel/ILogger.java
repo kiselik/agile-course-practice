31e5cf2f66cb6ed747cfef7eefcd91cfa9288482
Olga Kiseleva <olga.kiseleva@intel.com>
2019-12-26 18:00:44 +0300

    first naming was fixed

--- code/matrix/viewmodel/src/main/java/ru/unn/agile/Matrix/viewmodel/ILogger.java
+++ code/matrix/viewmodel/src/main/java/ru/unn/agile/Matrix/viewmodel/ILogger.java
@@ -3,7 +3,7 @@ package  ru.unn.agile.Matrix.viewmodel;
 import java.util.List;
 
 public interface ILogger {
-    void log(String s);
+    void logMessage(String s);
 
     List<String> getLog();
 }
